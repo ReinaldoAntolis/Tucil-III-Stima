01b20df0ca5b8e671ef69085ae37852c74966302
Reinaldo16519320 <sandyshroud00@gmail.com>
2021-04-06 22:40:34 +0800

    progress 2

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 # import Calculation
-from __main__ import *
+# from __main__ import *
+from math import radians, cos, sin, asin, sqrt
 
-def input(nama_file) : 
+def readFile(nama_file) : 
     simpul = []
     matrix = []
     with open(nama_file) as f:
@@ -34,22 +35,95 @@ def searchPos(namaTempat) :
     if(not found) : 
         return -1
 
-
 #change adjacency matrix from boolean to weighted
 def changeMatrix() : 
     for i in range(nodeCount) : 
         for j in range(i, nodeCount) : 
             if(matrix[i][j] != 0) : 
-                temp = haversine(simpul[i][0], simpul[i][1], simpul[j][0], simpul[j][1])
+                temp = haversine(searchPos(simpul[i][2]), searchPos(simpul[j][2]))
                 matrix[i][j] = temp
                 matrix[j][i] = temp
 
-simpul, matrix, nodeCount = input("ITB.txt")
-# changeMatrix()
-# for test in matrix :
-#     print(test)
+# Menghitung jarak antara dua titik di bumi dengan 
+# menggunakan garis lintang dan garis bujur
+def haversine(pos1, pos2):
+    # Mengubah titik desimal menjadi radian
+    lat1, lon1 = simpul[pos1][0], simpul[pos1][1]
+    lat2, lon2 = simpul[pos2][0], simpul[pos2][1]
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-print (aStar("Simpang_McD", "Tubagus_Ismail", matrix, nodeCount))
+    # Rumus perhitungan haversine
+    # r merupakan radius bumi dalam satuan kilometer
+    r = 6371
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    h = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    x = asin(sqrt(h)) 
+    result = 2 * x * r
+    return result
+
+def aStar (start, goal) :
+    shortestPath = []
+    shortestPath.append(start)
+    currNode = start
+    dFromStart = 0
+    while (currNode != goal and walkCount < nodeCount) :
+        min = None
+        minPos = None
+        isFirst = True
+        for i in range (nodeCount) :
+            if (matrix[searchPos(currNode)][i] != 0) :
+                if isFirst :
+                    isFirst = False
+                    minPos = i
+                    min = evalFunc(dFromStart, currNode, simpul[i][2], goal)
+                else :
+                    if (min > evalFunc(dFromStart, currNode, simpul[i][2], goal)) :
+                        minPos = i
+                        min = evalFunc(dFromStart, currNode, simpul[i][2], goal)
+        shortestPath.append(simpul[minPos][2])
+        dFromStart = gn(dFromStart, currNode, simpul[minPos][2]) 
+        currNode = simpul[minPos][2]
+    return dFromStart, shortestPath
+
+# Menghitung fungsi evaluasi dengan rumus f(n) = g(n) + h(n)
+def evalFunc (dFromStart, currNode, exploreNode, goal) :
+    fn = gn (dFromStart, currNode, exploreNode) + hn(exploreNode, goal)
+    return fn
+
+# g(n) merupakan fungsi yang menghitung total 
+# jarak dari simpul awal menuju exploreNode
+def gn (dFromStart, currNode, exploreNode) :
+    result = dFromStart + matrix[searchPos(currNode)][searchPos(exploreNode)]
+    return result
+
+# h(n) merupakan fungsi yang menghitung jarak 
+# garis lurus dari exploreNode menuju simpul tujuan    
+def hn (exploreNode, goal) :
+    return haversine (searchPos(exploreNode), searchPos(goal))
+
+
+# Main Program
+# Input file kasus uji
+kasusUji = input("Masukkan nama file kasus uji: ")
+# Mengolah input kasus uji
+simpul, matrix, nodeCount = readFile(kasusUji)
+# Mencetak semua simpul yang ada
+no = 1
+for node in simpul :
+    print(no, end="")
+    print(".", end="")
+    print(node[2])
+    no += 1
+# Mengubah adjacency matrix yang berisi boolean menjadi
+# berisi bobot antar simpul    
+changeMatrix()
+#Menerima input simpul asal dan simpul tujuan
+startNode = int(input("No simpul asal: "))
+goalNode = int(input("No simpul tujuan: "))
+
+distance, shortestPath = aStar(simpul[startNode - 1][2], simpul[goalNode - 1][2])
+print(shortestPath)
 
 
 
--- Visualization.py
+++ Visualization.py
@@ -1,5 +1,6 @@
 from flask import Flask
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return redirect("/web_scraping")
+	return "Hello uwu"
